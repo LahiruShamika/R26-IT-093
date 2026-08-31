23edadab2af4fe31a98e156eb281b76de0d08e7c
AmandaSharadee <sharadeeamanda74@gmail.com>
2026-08-31 15:05:58 +0530

    added new part

--- frontend/lib/models/feed_model.dart
+++ frontend/lib/models/feed_model.dart
@@ -743,7 +743,12 @@ class _FeedScreenState extends State<FeedScreen> {
     );
   }
 
-  {
+  Widget _buildFeedItem(
+      String label,
+      String value,
+      IconData icon,
+      ColorScheme cs,
+      ) {
     return Padding(
       padding:
       const EdgeInsets.symmetric(vertical: 12),
